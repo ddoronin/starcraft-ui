7b12d2a55f062262998b7c8605540537032291c0
Dima Doronin <doronindm@gmail.com>
2020-04-14 01:19:23 -0400

    Updates

---
+++ precache-manifest.fe478a07b6f2810836d171f518ba7a63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc7dd7b901766cfa8465f9d4679bc3cc",
+    "url": "/startcraft-ui/index.html"
+  },
+  {
+    "revision": "ec505a89509db97d09e8",
+    "url": "/startcraft-ui/static/css/main.b1c0e413.chunk.css"
+  },
+  {
+    "revision": "3f743c2cc1cf665168b3",
+    "url": "/startcraft-ui/static/js/2.bdac2cac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/startcraft-ui/static/js/2.bdac2cac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec505a89509db97d09e8",
+    "url": "/startcraft-ui/static/js/main.0b3a4361.chunk.js"
+  },
+  {
+    "revision": "bb0177c9aa99cb8780d4",
+    "url": "/startcraft-ui/static/js/runtime-main.e90a6423.js"
+  }
+]);
